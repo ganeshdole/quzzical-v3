1c12e08e1bf025c12f3edfd38c9a6c5f34ef4946
Ganesh Dole <94167146+ganeshdole@users.noreply.github.com>
2024-03-24 08:50:44 +0530

    Refactor CSS and Quiz component

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import Question from "../components/Question";
+
 export default function Quiz() {
   const [quizData, setQuizData] = useState([]);
-  const [countCorrectanswer, setCountCorrectanswer] = useState(0);
   const [showResult, setShowResult] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [newGame, setNewGame] = useState(false);
@@ -22,11 +22,6 @@ export default function Quiz() {
         if (question.questionId === questionId) {
           const updatedOptions = question.options.map((option) => {
             if (option.optionId === optionId) {
-              if (option.isCorrect) {
-                setCountCorrectanswer(
-                  (prevCountCorrectanswer) => prevCountCorrectanswer + 1
-                );
-              }
               return {
                 ...option,
                 isSelected: true,
@@ -50,7 +45,6 @@ export default function Quiz() {
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-      setCountCorrectanswer(0);
       try {
         const data = await fetch(
           "https://opentdb.com/api.php?amount=5&type=multiple"
@@ -87,38 +81,48 @@ export default function Quiz() {
     fetchData();
   }, [newGame]);
 
-  const questionElement = quizData.map((question) => {
-    return (
-      <Question
-        key={nanoid()}
-        question={question}
-        handleOptionSelect={handleOptionSelect}
-        showResult={showResult}
-      />
-    );
-  });
+  const questionElements = quizData.map((question) => (
+    <Question
+      key={question.questionId}
+      question={question}
+      handleOptionSelect={handleOptionSelect}
+      showResult={showResult}
+    />
+  ));
+
+  function countCorrectAnswers() {
+    let count = 0;
+    quizData.forEach((question) => {
+      const selectedOption = question.options.find(
+        (option) => option.isSelected
+      );
+      if (selectedOption && selectedOption.isCorrect) {
+        count++;
+      }
+    });
+    return count;
+  }
 
   return (
     <div>
-      {!isLoading && questionElement}
-      {isLoading && <h1>Loading...</h1>}
+      {!isLoading ? questionElements : <h1>Loading...</h1>}
       {!isLoading && !showResult && (
         <div className="card-footer">
-          <button onClick={() => setShowResult(true)}>check Answer</button>
+          <button onClick={() => setShowResult(true)}>Check Answer</button>
         </div>
       )}
       {showResult && (
         <p className="score-card" style={{ margin: "5px" }}>
-          You got {countCorrectanswer}/ {quizData.length} correct
+          You got {countCorrectAnswers()}/{quizData.length} correct
         </p>
       )}
       {showResult && (
         <div className="card-footer">
           <button
             onClick={() =>
-              setNewGame((preValue) => {
-                setShowResult((preValue) => !preValue);
-                return !preValue;
+              setNewGame((prevValue) => {
+                setShowResult((prevValue) => !prevValue);
+                return !prevValue;
               })
             }
           >
