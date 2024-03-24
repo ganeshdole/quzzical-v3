86ff68cc56dca203507f618a07db2ab4a694b9a6
Ganesh Dole <94167146+ganeshdole@users.noreply.github.com>
2024-03-24 08:27:55 +0530

    Update CSS styles and Question component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -6,21 +6,45 @@ export default function Question(props) {
   const [selectedAnswer, setSelectedAnswer] = React.useState(null);
 
   const handleAnswerChange = (event) => {
-    setSelectedAnswer(event.target.value);
+    if (!props.showResult) setSelectedAnswer(event.target.value);
+  };
+
+  const handleLabelClick = (event) => {
+    if (event.target.htmlFor && !props.showResult)
+      props.handleOptionSelect(props.question.questionId, event.target.htmlFor);
   };
 
   const answerOptions = props.question.options.map((options) => {
+    let className = `option ${options.isSelected ? "selected" : ""}`;
+
+    if (props.showResult) {
+      if (options.isSelected && options.isCorrect) {
+        className += " correct";
+      } else if (options.isSelected && !options.isCorrect) {
+        className += " incorrect";
+      }
+      if (options.isCorrect) {
+        className += " correct";
+      }
+    }
+
     return (
       <div key={nanoid()} id={props.question.questionId}>
-        <input
-          type="radio"
-          id={options.optionId}
-          name={options.optionId}
-          value={options.option}
-          checked={selectedAnswer === options.option}
-          onChange={handleAnswerChange}
-        />
-        <label htmlFor={options.optionId}>{he.decode(options.option)}</label>
+        <label
+          htmlFor={options.optionId}
+          onClick={handleLabelClick}
+          className={className}
+        >
+          <input
+            type="radio"
+            id={options.optionId}
+            name={options.optionId}
+            value={options.option}
+            checked={selectedAnswer === options.option}
+            onChange={handleAnswerChange}
+          />
+          {he.decode(options.option)}
+        </label>
       </div>
     );
   });
